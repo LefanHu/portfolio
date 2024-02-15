ce6a6a91d1d8b2ccac35b408218264c993612da9
Lefan Hu <lefanhu1@gmail.com>
2024-02-14 22:31:23 -0500

    confetti explosions

--- app/(meilin)/silly-goose/page.tsx
+++ app/(meilin)/silly-goose/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import Explosion from "react-canvas-confetti/dist/presets/fireworks";
 
 export default function SillyGoose() {
   const clipPathStyle: React.CSSProperties = {
@@ -8,9 +11,12 @@ export default function SillyGoose() {
   };
 
   return (
-    <div className="bg-white">
+    <div className="bg-white h-full overflow-clip">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               {/* <span className="sr-only">Your Company</span> */}
@@ -23,49 +29,34 @@ export default function SillyGoose() {
               {/* /> */}
             </a>
           </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            </button>
-          </div>
           <div className="hidden lg:flex lg:gap-x-12">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
               Wat is this?
             </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
               Image Gallery
             </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
               Swift Generator
             </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
               How did I make this?
             </a>
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900"> */}
-            {/*   Log in <span aria-hidden="true">&rarr;</span> */}
-            {/* </a> */}
-          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
         </nav>
-        {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -80,23 +71,23 @@ export default function SillyGoose() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true"></span>Read more{" "}
-                <span aria-hidden="true">&rarr;</span>
-              </a>
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10">
+              Here&apos;s a little something special for you...
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Happy Birthday!
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">My silly goose is 20? Whoa...</p>
+            <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
+              <h1 className="p-2 text-6xl font-bold tracking-tight">
+                Happy Birthday!
+              </h1>
+            </div>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              My silly goose is 20? Whoa...
+            </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="z-11 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Open Your Birthday Present
               </a>
@@ -104,7 +95,7 @@ export default function SillyGoose() {
           </div>
         </div>
         <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
           <div
@@ -113,6 +104,16 @@ export default function SillyGoose() {
           ></div>
         </div>
       </div>
+      {
+        <Explosion
+          autorun={{ speed: 1, delay: 1000 }}
+          decorateOptions={(options) => {
+            options.gravity = 1;
+            return options;
+          }}
+          globalOptions={{ useWorker: true }}
+        />
+      }
     </div>
   );
 }
