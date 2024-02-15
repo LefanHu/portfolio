a87a0f875a2ccf64b593158f376f21da6c8eee4e
Lefan Hu <lefanhu1@gmail.com>
2024-02-15 17:42:29 -0500

    forbid screen size too small

--- app/(meilin)/silly-goose/page.tsx
+++ app/(meilin)/silly-goose/page.tsx
@@ -1,15 +1,38 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import Explosion from "react-canvas-confetti/dist/presets/fireworks";
 
 export default function SillyGoose() {
+  const router = useRouter();
   const clipPathStyle: React.CSSProperties = {
     clipPath:
       "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Check if window is available since Next.js does server-side rendering
+      if (typeof window !== "undefined") {
+        const screenWidth: number = window.innerWidth;
+
+        // Redirect if screen width is less than or equal to 768 pixels
+        if (screenWidth <= 768) {
+          router.push("/silly-goose/warn"); // Change '/another-page' to your desired redirection target
+        }
+      }
+    };
+
+    // check screen size immediately
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove event listener
+    return () => window.removeEventListener("resize", handleResize);
+  });
 
   return (
     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
