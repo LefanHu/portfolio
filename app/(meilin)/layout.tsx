6eeb4fca4cdb98d55c2f5a71f55c4d585c324ca6
Lefan Hu <lefanhu1@gmail.com>
2024-02-15 17:25:47 -0500

    convert background and nav to layout

--- app/(meilin)/layout.tsx
+++ app/(meilin)/layout.tsx
@@ -1,26 +1,86 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '../globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lefan's Website",
-  description: 'Portfolio website showcasing various projects & algorithms',
-}
+  description: "Portfolio website showcasing various projects & algorithms",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const clipPathStyle: React.CSSProperties = {
+    clipPath:
+      "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+  };
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='flex flex-col h-screen box-border'>
-          {children}
+        <div className="bg-white h-screen overflow-clip">
+          <header className="absolute inset-x-0 top-0 z-50">
+            <nav
+              className="flex items-center justify-between p-6 lg:px-8"
+              aria-label="Global"
+            >
+              <div className="flex lg:flex-1"></div>
+              <div className="hidden md:flex md:gap-x-10 lg:flex lg:gap-x-12">
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Wat is this?
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Image Gallery
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Swift Generator
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  How did I make this?
+                </a>
+              </div>
+              <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+            </nav>
+          </header>
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={clipPathStyle}
+              ></div>
+            </div>
+            <div className="flex flex-col h-screen box-border">{children}</div>
+            <div
+              className="absolute -z-10 inset-x-0 top-[calc(100%-13rem)] transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                style={clipPathStyle}
+              ></div>
+            </div>
+          </div>
         </div>
       </body>
     </html>
-  )
+  );
 }
--- app/(meilin)/silly-goose/page.tsx
+++ app/(meilin)/silly-goose/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
 import Explosion from "react-canvas-confetti/dist/presets/fireworks";
 
@@ -11,98 +12,28 @@ export default function SillyGoose() {
   };
 
   return (
-    <div className="bg-white h-full overflow-clip">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              {/* <span className="sr-only">Your Company</span> */}
-              {/* <Image */}
-              {/*   className="h-8 w-auto" */}
-              {/*   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" */}
-              {/*   alt="" */}
-              {/*   width={500} */}
-              {/*   height={500} */}
-              {/* /> */}
-            </a>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Wat is this?
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Image Gallery
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Swift Generator
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              How did I make this?
-            </a>
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
-        </nav>
-      </header>
-
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={clipPathStyle}
-          ></div>
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10">
+          Here&apos;s a little something special for you...
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10">
-              Here&apos;s a little something special for you...
+      </div>
+      <div className="text-center">
+        <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
+          <h1 className="p-2 text-6xl font-bold tracking-tight">
+            Happy Birthday!
+          </h1>
+        </div>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          My silly goose is 20? Whoa...
+        </p>
+        <Link href="/silly-goose/intro" className="z-51">
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div className="z-11 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Open Your Birthday Present
             </div>
           </div>
-          <div className="text-center">
-            <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
-              <h1 className="p-2 text-6xl font-bold tracking-tight">
-                Happy Birthday!
-              </h1>
-            </div>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              My silly goose is 20? Whoa...
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="z-11 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Open Your Birthday Present
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={clipPathStyle}
-          ></div>
-        </div>
+        </Link>
       </div>
       {
         <Explosion
