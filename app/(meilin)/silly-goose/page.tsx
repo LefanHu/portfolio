edb2b149a8a6e7303836961393a523970cd0ec4e
Lefan Hu <lefanhu1@gmail.com>
2024-02-16 13:18:27 -0500

    initial easter egg page

---
+++ app/(meilin)/silly-goose/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function SwiftGallery() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Check if window is available since Next.js does server-side rendering
+      if (typeof window !== "undefined") {
+        const screenWidth: number = window.innerWidth;
+
+        // Redirect if screen width is less than or equal to 768 pixels
+        if (screenWidth <= 768) {
+          router.push("/silly-goose/warn"); // Change '/another-page' to your desired redirection target
+        }
+      }
+    };
+
+    // check screen size immediately
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove event listener
+    return () => window.removeEventListener("resize", handleResize);
+  });
+  return (
+    <div className="text-black">easter egg! hehe</div>
+  );
+}
--- app/(meilin)/silly-goose/simp/page.tsx
+++ app/(meilin)/silly-goose/simp/page.tsx
@@ -1,3 +1,8 @@
 export default function Simp() {
-  return <div className="text-black">with a lot of time and love...</div>;
+  return (
+    <div className="text-black">
+      with a lot of time and love... PS. there&apos;s an easter egg somewhere
+      <div className="text-white">HINT: maybe there&apos;s a part of the link you never visted?</div>
+    </div>
+  );
 }
