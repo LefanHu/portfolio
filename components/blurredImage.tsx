2fb5a743a63750ec7a10545c6f482de8675e5ce8
Lefan Hu <lefanhu1@gmail.com>
2024-02-24 01:12:18 -0500

    add button for copying prompts

--- components/blurredImage.tsx
+++ components/blurredImage.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  ClipboardDocumentIcon,
+  ClipboardIcon,
+} from "@heroicons/react/24/outline";
 
 interface BlurredImageData {
   src?: string;
@@ -26,35 +30,44 @@ export default function BlurredImage({
   const [isLoading, setLoading] = useState(true);
 
   const originalCaption: string = caption;
-  if (caption.length > 65) {
-    caption = caption.slice(0, 63) + "...";
+  if (caption.length > 85) {
+    caption = caption.slice(0, 83) + "...";
   }
 
   // TODO: add button to copy full prompt
 
   return (
-    <a
-      onClick={() => copyToClipboard(originalCaption)}
-      href={src}
-      target="_blank"
-      className="group"
-    >
-      <div className=" backdrop-blur-md p-2 max-w-sm rounded-lg overflow-hidden shadow-lg hover:outline-fuchsia-100 hover:outline-dashed transition-all ease-in-out">
-        <Image
-          alt=""
-          src={src}
-          width={500}
-          height={500}
-          className={cn(
-            "group-hover:opacity-75 duration-300 ease-in-out rounded-lg",
-            isLoading
-              ? "grayscale blur-lg scale-105"
-              : "grayscale-0 blur-0 scale-100"
-          )}
-          onLoad={() => setLoading(false)}
-        />
+    <div className="group h-full">
+      <div className="backdrop-blur-md p-2 max-w-sm rounded-lg overflow-hidden shadow-lg hover:outline-fuchsia-100 hover:outline-dashed transition-all ease-in-out h-full">
+        <a href={src} target="_blank">
+          <Image
+            alt=""
+            src={src}
+            width={500}
+            height={500}
+            className={cn(
+              "group-hover:opacity-75 duration-300 ease-in-out rounded-lg",
+              isLoading
+                ? "grayscale blur-lg scale-105"
+                : "grayscale-0 blur-0 scale-100"
+            )}
+            onLoad={() => setLoading(false)}
+            onClick={() => copyToClipboard(originalCaption)}
+          />
+        </a>
+        <div className="flex items-center justify-between space-x-2 z-50 flex-grow">
+          <span className="text-sm text-gray-700">{caption}</span>
+          <button
+            onClick={() => {
+              copyToClipboard(originalCaption);
+              alert("prompt copied to clipboard");
+            }}
+            className="m-3 aspect-square h-12 max-h-[36px] w-12 max-w-[36px] rounded-lg bg-pink-500 font-sans text-sm font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            <ClipboardDocumentIcon className="p-1 text-2xl leading-none" />
+          </button>
+        </div>
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">{caption}</h3>
-    </a>
+    </div>
   );
 }
