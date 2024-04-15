f8a20266a2adf3c0a21a7ef7a19510edfbb4658e
Lefan Hu <lefanhu1@gmail.com>
2024-04-15 12:15:57 -0400

    migrate to aws v3 js

--- app/api/s3/buckets/[bucket]/files/route.tsx
+++ app/api/s3/buckets/[bucket]/files/route.tsx
@@ -1,25 +1,17 @@
-import AWS from "aws-sdk";
-// import * as AWS from "@aws-sdk/client-s3";
-// const client = new AWS.S3({ region: "REGION" });
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
-// Configure AWS
-AWS.config.update({
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
-const s3 = new AWS.S3();
+const client = new S3Client({});
 
 export async function GET(req: Request, { params }: { params: { bucket: string } }) {
-  try {
-    const options = {
-      Bucket: params.bucket,
-    };
+  console.log("sneak attacs requested!!!");
+  const command = new ListObjectsV2Command({
+    Bucket: params.bucket,
+  });
 
-    // Fetch the list of files from the specified bucket
-    const data = await s3.listObjectsV2(options).promise();
-    return new Response(JSON.stringify(data.Contents), {
+  try {
+    // default truncation is 1000 keys, which we are not reaching
+    const { Contents, IsTruncated, NextContinuationToken } = await client.send(command);
+    return new Response(JSON.stringify(Contents), {
       status: 200,
     });
   } catch (error) {
