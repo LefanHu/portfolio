02bd14caa8e8e89b4cf1aee4c8d3aba97c2e9b34
Lefan Hu <lefanhu1@gmail.com>
2024-04-22 10:04:53 -0400

    final sneak attac update

--- app/(meilin)/silly-goose/page.tsx
+++ app/(meilin)/silly-goose/page.tsx
@@ -6,13 +6,30 @@ export default function SwiftGallery() {
     <div>
       <div className="text-black">easter egg! hehe</div>
       <div className="text-black">
-        spiritually... i&apos;ll always be the first to wish you a happy birthday
+        {/* spiritually... i&apos;ll always be the first to wish you a happy
+        birthday */}
+        update: happy birthday!
       </div>
-      <p>(p.s. i finished it)</p>
+      {/* <p className="text-black">(p.s. i finished it)</p> */}
 
-      <Link className="text-blue-500 underline" href="/silly-goose/sneak-attac">
-        sneak attac!!!
-      </Link>
+      <div className=" relative">
+        <div>
+          <Link
+            className="text-blue-500 underline"
+            href="/silly-goose/sneak-attac"
+          >
+            sneak attac!!!
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="text-blue-500 underline"
+            href={process.env.NEXT_PUBLIC_SNEAK_ATTAC_2!}
+          >
+            sneak attac again!!!
+          </Link>
+        </div>
+      </div>
       <RunawayButton href="/silly-goose/happy-birthday" />
     </div>
   );
