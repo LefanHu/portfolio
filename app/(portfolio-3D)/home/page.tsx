aced4d25a4758afd5196207584b1a6829637845d
Lefan Hu <lefanhu1@gmail.com>
2024-09-02 19:07:26 -0400

    rotation quaternions working

--- app/(portfolio-3D)/home/page.tsx
+++ app/(portfolio-3D)/home/page.tsx
@@ -2,60 +2,61 @@
 
 import * as THREE from "three";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
-import { easing } from "maath";
 import {
   Environment,
   OrbitControls,
   AccumulativeShadows,
   RandomizedLight,
-  Bvh,
-  Html,
-  MotionPathControls,
   PerspectiveCamera,
-  useHelper,
-  ContactShadows,
 } from "@react-three/drei";
-import { EffectComposer, Selection, Outline } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  Selection,
+  Outline,
+} from "@react-three/postprocessing";
 
 import { Suspense, use, useCallback, useEffect, useRef, useState } from "react";
 import { PortfolioScene } from "@/components/PortfolioScene";
-import NextUICard from "@/components/NextUICard";
 
 import { useCameraStore } from "@/lib/store";
-import { easeIn } from "framer-motion";
-import { Geometries } from "@/components/three/Geometries";
+import { Box } from "@/components/Box";
 
 const startCameraPosition: [number, number, number] = [0.5, 0, 5];
 // const viewCameraPosition: [number, number, number] = [2, 0.5, 1.5];
 
-const viewCameraPosition: [number, number, number] = [0.5, 1, 5];
+const viewCameraPosition: [number, number, number] = [5, 1, -3];
 
 function Camera() {
   const cameraRef = useRef<THREE.PerspectiveCamera>(null);
-  const { targetPosition, targetRotation } = useCameraStore();
+  const { targetPosition, targetLookat } = useCameraStore();
 
   useFrame((state, delta) => {
-    const camera = state.camera;
+    const camera = cameraRef.current;
+    if (!camera) return;
 
     // Animate position
     // easing.damp3(camera.position, targetPosition, 5, delta);
-    camera.position.lerp(new THREE.Vector3(...targetPosition), 0.05 * delta * 10);
+    camera.position.lerp(new THREE.Vector3(...targetPosition), delta);
 
-    // Get current camera rotation
-    const target = new THREE.Vector3(...targetRotation);
+    const target = new THREE.Vector3(...targetLookat);
 
     // Calculate the new quaternion for smooth rotation
-    const quaternion = new THREE.Quaternion();
-    const direction = new THREE.Vector3().subVectors(target, camera.position).normalize();
+    const direction = new THREE.Vector3()
+      .subVectors(target, camera.position)
+      .normalize();
 
-    // Set the new rotation quaternion to smoothly rotate towards target
-    quaternion.setFromUnitVectors(camera.getWorldDirection(new THREE.Vector3()), direction);
-    camera.quaternion.slerp(quaternion, 0.05 * delta * 10); // Smoothly rotate with slerp
+    // calculate target quat
+    var qt = new THREE.Quaternion().setFromUnitVectors(
+      new THREE.Vector3(0, 0, -1),
+      direction
+    );
 
-    // camera.lookAt(target);
+    camera.quaternion.slerp(qt, 2 * delta); // Smoothly rotate with slerp
   });
 
-  return <PerspectiveCamera makeDefault ref={cameraRef} position={[0.5, 0, 5]} />;
+  return (
+    <PerspectiveCamera makeDefault ref={cameraRef} position={[0.5, 0, 5]} />
+  );
 }
 
 function CameraDebugger(props: JSX.IntrinsicElements["cameraHelper"]) {
@@ -70,12 +71,26 @@ function CameraDebugger(props: JSX.IntrinsicElements["cameraHelper"]) {
 }
 
 export default function Home3DPage() {
-  const cameraDebuggerRef = useRef<THREE.CameraHelper>(null);
-  const boxRef = useRef<THREE.Mesh>(null);
+  // const cameraDebuggerRef = useRef<THREE.CameraHelper>(null);
+  // const boxRef = useRef<THREE.Mesh>(null);
+
+  useEffect(() => {
+    // delay 1 second then move camera
+    setTimeout(() => {
+      useCameraStore.setState({ targetPosition: viewCameraPosition });
+      useCameraStore.setState({ targetLookat: [-1, 1, 8] });
+    }, 2000);
+  }, []);
+
   return (
     <div className="w-screen h-screen">
-      <Canvas shadows className="bg-white" camera={{ position: [0.5, 0, 5], fov: 55 }}>
-        {/* <Camera /> */}
+      <Canvas
+        shadows
+        className="bg-white"
+        // camera={{ position: [0.5, 0, 5], fov: 55 }}
+      >
+        <Box position={[-3, 1, 14]} />
+        <Camera />
         {/* <Box args={[1, 1, 1]} position={[0, 0, 0]} ref={boxRef} /> */}
         {/* <CameraDebugger ref={cameraDebuggerRef} /> */}
         <Suspense fallback={null} />
@@ -86,36 +101,20 @@ export default function Home3DPage() {
           alphaTest={0.75}
           opacity={0.9}
         >
-          <RandomizedLight radius={6} position={[5, 5, 10]} bias={0.001} castShadow />
+          <RandomizedLight
+            radius={6}
+            position={[5, 5, 10]}
+            bias={0.001}
+            castShadow
+          />
         </AccumulativeShadows>
-
-        {/* <MotionPathControls
-          offset={0}
-          damping={0.2}
-          curves={[
-            new THREE.CubicBezierCurve3(
-              new THREE.Vector3(-5, -5, 0),
-              new THREE.Vector3(-10, 0, 0),
-              new THREE.Vector3(0, 3, 0),
-              new THREE.Vector3(6, 3, 0)
-            ),
-            new THREE.CubicBezierCurve3(
-              new THREE.Vector3(6, 3, 0),
-              new THREE.Vector3(10, 5, 5),
-              new THREE.Vector3(5, 3, 5),
-              new THREE.Vector3(5, 5, 5)
-            ),
-          ]}
-          attach
-        ></MotionPathControls> */}
 
         <Selection>
           <PortfolioScene position={[1, -0.5, 0]} />
           {/* <Effects /> */}
         </Selection>
 
-        <Geometries />
-        {/* <ContactShadows position={[0, -9, -10]} opacity={0.7} scale={40} blur={1} /> */}
+        {/* <Geometries /> */}
 
         {/* <Html
           transform
@@ -126,7 +125,7 @@ export default function Home3DPage() {
           <NextUICard></NextUICard>
         </Html> */}
 
-        <OrbitControls
+        {/* <OrbitControls
           enablePan={false}
           enableZoom={true}
           minPolarAngle={Math.PI / 8}
@@ -135,7 +134,7 @@ export default function Home3DPage() {
           minAzimuthAngle={-Math.PI / 3}
           minDistance={1}
           maxDistance={8}
-        />
+        /> */}
         <Environment preset="city" />
 
         <Suspense />
--- lib/store.tsx
+++ lib/store.tsx
@@ -4,7 +4,7 @@ interface CameraState {
   position: [number, number, number];
   targetPosition: [number, number, number];
   rotation: [number, number, number];
-  targetRotation: [number, number, number];
+  targetLookat: [number, number, number];
   setCameraPosition: (newPosition: [number, number, number]) => void;
   setCameraLookat: (newRotation: [number, number, number]) => void;
 }
@@ -13,7 +13,7 @@ export const useCameraStore = create<CameraState>()((set) => ({
   position: [0.5, 0, 5], // Initial camera position
   targetPosition: [0.5, 0, 5],
   rotation: [1, 0.5, 1], // Initial camera lookat target
-  targetRotation: [1, 0.5, 1],
+  targetLookat: [1, 0.5, 1],
 
   // Action to update camera position smoothly
   setCameraPosition: (newPosition) =>
@@ -24,6 +24,6 @@ export const useCameraStore = create<CameraState>()((set) => ({
   // Action to update camera rotation smoothly
   setCameraLookat: (newRotation) =>
     set(() => ({
-      targetRotation: newRotation, // target rotation for animation
+      targetLookat: newRotation, // target rotation for animation
     })),
 }));
