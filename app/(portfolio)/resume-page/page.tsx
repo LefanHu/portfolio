e2deb73bfd4627305a5db9be8bc32f2e810009f8
Lefan Hu <lefanhu1@gmail.com>
2024-10-08 17:22:49 -0400

    update resume

--- app/(portfolio)/resume-page/page.tsx
+++ app/(portfolio)/resume-page/page.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
   return (
     <div className="flex-1 overflow-auto no-scrollbar">
       <iframe
-        src="/2a-main_resume.pdf"
+        src="/3a-main_resume.pdf"
         className="h-full w-full min-h-screen"
       />
     </div>
