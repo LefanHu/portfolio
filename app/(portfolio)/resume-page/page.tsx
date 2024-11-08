569fe15c562319a95180bfb31bca0382def13d9c
Lefan Hu <lefanhu1@gmail.com>
2024-11-07 21:58:04 -0500

    remove transcript from resume

--- app/(portfolio)/resume-page/page.tsx
+++ app/(portfolio)/resume-page/page.tsx
@@ -1,10 +1,7 @@
 export default function Resume() {
   return (
     <div className="flex-1 overflow-auto no-scrollbar">
-      <iframe
-        src="/3a-external-package.pdf"
-        className="h-full w-full min-h-screen"
-      />
+      <iframe src="/3a-external.pdf" className="h-full w-full min-h-screen" />
     </div>
   );
 }
