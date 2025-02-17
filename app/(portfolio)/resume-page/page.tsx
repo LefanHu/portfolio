a3d23f1a520e6dac06a72e2fd5dff6836e616df0
Lefan Hu <lefanhu1@gmail.com>
2025-02-16 19:42:03 -0500

    update resume

--- app/(portfolio)/resume-page/page.tsx
+++ app/(portfolio)/resume-page/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
   return (
     <div className="flex-1 overflow-auto no-scrollbar">
-      <iframe src="/3a-external.pdf" className="h-full w-full min-h-screen" />
+      <iframe src="/3a-resume-hardware.pdf" className="h-full w-full min-h-screen" />
     </div>
   );
 }
