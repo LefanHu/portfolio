84180021a1f1d4e49d32a7d6ac3511e563ce9ceb
Lefan <56052054+LefanHu@users.noreply.github.com>
2026-03-28 13:03:30 -0400

    remove meilin/mongodb feature set and related auth/api routes

--- app/(portfolio-3D)/layout.tsx
+++ app/(portfolio-3D)/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lefan's Website",
@@ -15,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
